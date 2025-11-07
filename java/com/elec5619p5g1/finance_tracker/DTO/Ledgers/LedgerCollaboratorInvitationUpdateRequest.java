b9ddb6dc1b8f5867d7e86b8b347b0e4cb4026041
ccc-llz <chiale1123@gmail.com>
2025-11-07 19:08:31 +1100

    Delete java/com/elec5619p5g1/finance_tracker directory

--- java/com/elec5619p5g1/finance_tracker/DTO/Ledgers/LedgerCollaboratorInvitationUpdateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.elec5619p5g1.finance_tracker.DTO.Ledgers;
-
-import lombok.Data;
-
-@Data
-public class LedgerCollaboratorInvitationUpdateRequest {
-    String operation; // REVOKE, RETRY, SET
-    Integer permission; // 0 - READONLY, 1 - EDITABLE
-}
--- java/com/elec5619p5g1/finance_tracker/Enum/LedgerPermissionType.java
+++
@@ -1,8 +0,0 @@
-package com.elec5619p5g1.finance_tracker.Enum;
-
-public enum LedgerPermissionType {
-    EDITABLE,
-    READONLY;
-
-    LedgerPermissionType(){}
-}
